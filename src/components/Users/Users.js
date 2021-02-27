8e27664a7430af0db9b43a0fb70d4eeef33f0462
Rakib544 <md.rakib10122003@gmail.com>
2021-02-27 09:03:11 +0600

    new files added

---
+++ src/components/Users/Users.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faPlus} />
+
+const Users = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Users;
